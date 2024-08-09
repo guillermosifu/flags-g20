f495a4fe258bfe083cd6f3764e67464efda0d1af
guillermosifu <sifuentes1201@gmail.com>
2024-08-08 21:05:01 -0500

    ol

--- src/Flags/Flags.jsx
+++ src/Flags/Flags.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card,CardContent,CardMedia,Container,Grid,TextField,
     FormControl,InputLabel,Select,MenuItem
  } from '@mui/material'
+import { getFlags } from '../service'
 
 
 const Flags = () => {
+ const[countries,setCountries]=useState([]);
+ const[region,setRegion]=useState("")
+
+const fetchCountries=async(url)=>{
+    const response = await getFlags(url)
+    setCountries(response)
+   console.log(response)
+}
+
+const handleRegion =async (e)=>{
+setRegion(e.target.value);
+if(e.target.value === "all"){
+    fetchCountries();
+    return
+}
+setCountries([])
+}
+
+
+
+
+
+
   return (
     <Container>
         <Grid container spacing={3} mt={5}>
@@ -15,11 +39,12 @@ const Flags = () => {
          <FormControl fullWidth>
          <InputLabel>Filter by Region</InputLabel>
          <Select label="Busca Region" fullWidth >
-          <MenuItem value={"africa"}>Africa</MenuItem>
-          <MenuItem value={"america"}>America</MenuItem>
-          <MenuItem value={"asia"}>Asia</MenuItem>
-          <MenuItem value={"europe"}>Europe</MenuItem>
-          <MenuItem value={"oceania"}>Oceania</MenuItem>   
+         <MenuItem value="all">Todas les regiones</MenuItem> 
+          <MenuItem value="africa">Africa</MenuItem>
+          <MenuItem value="america">America</MenuItem>
+          <MenuItem value="asia">Asia</MenuItem>
+          <MenuItem value="europe">Europe</MenuItem>
+          <MenuItem value="oceania">Oceania</MenuItem>   
             </Select>
             </FormControl>
          </Grid>
