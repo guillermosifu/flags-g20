01fe153b4433578962c0e1d0840e6e3c777b1ce4
guillermosifu <sifuentes1201@gmail.com>
2024-08-08 20:17:11 -0500

    selectt

--- src/Flags/Flags.jsx
+++ src/Flags/Flags.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card,CardContent,CardMedia,Container,Grid,TextField,
-    FormControl,InputLabel,Select
+    FormControl,InputLabel,Select,MenuItem
  } from '@mui/material'
 
 
@@ -11,8 +11,17 @@ const Flags = () => {
          <Grid item md={6}>
          <TextField label="Busca tu pais" fullWidth />
          </Grid>
-        </Grid>
-      
+         <Grid item md={6}>
+         <Select label="Busca Region" fullWidth >
+          <MenuItem value={"africa"}>Africa</MenuItem>
+          <MenuItem value={"america"}>America</MenuItem>
+          <MenuItem value={"asia"}>Asia</MenuItem>
+          <MenuItem value={"europe"}>Europe</MenuItem>
+          <MenuItem value={"oceania"}>Oceania</MenuItem>
+   
+            </Select>
+         </Grid>
+        </Grid>      
     </Container>
   )
 }
